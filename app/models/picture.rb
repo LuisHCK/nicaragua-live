9213a91d2a44cf0831c4a0b0c180cfbf675cfce6
LuisHCK <luisjcenteno@live.com>
2015-12-20 22:43:41 -0600

    resize pic to 600px

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,7 @@
 class Picture < ActiveRecord::Base
 	belongs_to :post
 
-	has_attached_file :image, styles: {tumb: "600x600#"}
-
+	has_attached_file :image, styles: {thumb: "600x600#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/ , presence: true
 
 
