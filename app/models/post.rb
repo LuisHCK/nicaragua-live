34954ca583ef38ffb659360997995d56114c94f6
LuisHCK <luisjcenteno@live.com>
2016-01-10 22:41:51 +0100

    validate if post is empty

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
+	validates :body, presence: true
 
 	has_many :pictures, :dependent => :destroy
 
 	belongs_to :profile
-	
+
 	has_many :hearts, dependent: :destroy
   has_many :users, through: :hearts
 end
