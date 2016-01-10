1dc602779e7f58aae4066218890dddb23eb13ebb
LuisHCK <luisjcenteno@live.com>
2016-01-10 22:34:31 +0100

    Validate text
    If text field is empty don't allow to post

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -14,3 +14,22 @@ $(".show-more a").on("click", function() {
 
     $this.text(linkText);
 });
+
+///validate length of the post
+function trimfield(str)
+{
+    return str.replace(/^\s+|\s+$/g,'');
+}
+
+function validate_form()
+  {
+       var obj1 = document.input_body.text_area_body;
+           if(trimfield(obj1.value) == '')
+           {
+             alert("Please Provide Details!");
+             obj1.focus();
+             return false;
+           }
+           else
+             return true;
+  }
