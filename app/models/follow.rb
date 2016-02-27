0f5386b1e52b7899b3775963c2506378cf2e5e46
LuisHCK <luisjcenteno@live.com>
2016-02-27 16:50:31 +0100

    remove validation
    An extra validation cause the user can follow only one profile

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,5 +3,5 @@ class Follow < ActiveRecord::Base
   belongs_to :profile
 
   validates :profile_id, uniqueness: {scope: :profile_id}
-  validates :clientprofile_id, uniqueness: {scope: :clientprofile_id}  
+  #validates :clientprofile_id, uniqueness: {scope: :clientprofile_id}  
 end
