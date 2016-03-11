0e80c7564286d31564db478b079e88e9f8829405
LuisHCK <luisjcenteno@live.com>
2016-03-11 04:14:50 +0100

    Add new size for cover img

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ActiveRecord::Base
 	has_many :posts
 	has_attached_file :cover, styles: {card: "600x600#"}
+	has_attached_file :cover, styles: {strech: "600x300#"}
 	has_attached_file :avatar, styles: {mini: "400x400#"}
 	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/ , presence: true
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/ , presence: true
