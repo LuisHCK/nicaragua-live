3efd699c1b0a1c8d6e61b492a74820459426201d
LuisHCK <luisjcenteno@live.com>
2016-03-16 01:45:50 -0600

    Show post from followed users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,11 @@ class WelcomeController < ApplicationController
        @categories = Category.all
        @profiles = Profile.all
        @clientprofiles = Clientprofile.all
-       @posts = Post.order(created_at: :desc).all
+
        @post = Post.new
        @follows = Follow.all
+       @posts = Post.where(:profile_id => current_client.clientprofile.follows.collect(&:profile_id) ).order(created_at: :desc).all
+       #this line collect the ids from followed users, then compare with the id of the post's owner and show in to welcome page. (an entire night for make this lol)
   end
 
 
