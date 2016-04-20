0fe1b96431309f96f80a04fd06b8b97cab6e977f
LuisHCK <luisjcenteno@live.com>
2016-04-20 00:09:32 -0600

    Fix ERROR SystemStackError: stack level too deep
    Remove: dependent: :destroy fix the error

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,6 @@
 class Follow < ActiveRecord::Base
-  belongs_to :clientprofile, dependent: :destroy
-  belongs_to :profile, dependent: :destroy
+  belongs_to :clientprofile
+  belongs_to :profile
 
   validates :profile_id, uniqueness: {scope: :profile_id}
   #validates :clientprofile_id, uniqueness: {scope: :clientprofile_id}
