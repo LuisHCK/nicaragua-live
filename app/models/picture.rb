0faab75ce64b94622444349605091259afa5936e
LuisHCK <luisjcenteno@live.com>
2016-04-20 00:09:54 -0600

    Reduce image quality

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,7 +4,7 @@ class Picture < ActiveRecord::Base
 	has_attached_file :image, styles: { thumb: ["64x64#", :jpg],
 						original: ['600', :jpg] },
 	convert_options: { thumb: "-quality 85 -strip",
-										 original: "-quality 95 -strip" }
+										 original: "-quality 90 -strip" }, dependent: :destroy
 
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/ , presence: true
 
