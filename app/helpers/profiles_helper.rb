f7389c6b3a5260424fb313ea6b8b2322d52ac893
LuisHCK <luisjcenteno@live.com>
2016-06-21 17:01:36 -0600

    update views

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -2,4 +2,10 @@ module ProfilesHelper
 	def get_post_count
 		current_user.posts.count
 	end 
+
+	def editable?(profile)
+		if client_signed_in? && profile.id == current_user.profile.id
+		 return true
+		end
+	end
 end
