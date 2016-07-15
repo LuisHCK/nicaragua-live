a632b31da03ced83fdd620bb03868320bdf9e80e
LuisHCK <luisjcenteno@live.com>
2016-07-14 18:09:22 -0600

    fix?

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,6 +78,7 @@ namespace :deploy do
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
+  after "deploy", "deploy:restart_daemons" 
 end
 
 # ps aux | grep puma    # Get puma pid
