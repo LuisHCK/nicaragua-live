dff437f9f5fec7445f39808939d729a0e7d93214
LuisHCK <luisjcenteno@live.com>
2016-07-27 20:34:39 -0600

    fix humanize date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -69,7 +69,7 @@ end
 
 def human_date(in_human_date)
     #in_human_date.date.strftime()
-    I18n.l(in_human_date.date, format: "%A %d %B, %Y")
+    I18n.l(in_human_date, format: "%A %d %B, %Y")
   end
 
   def human_time(in_human_time)
