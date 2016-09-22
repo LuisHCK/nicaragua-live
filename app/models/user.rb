6bb09dcb0971ba7f1296763f1f9de4ce9b835e3a
LuisHCK <luisjcenteno@live.com>
2016-09-22 16:19:32 -0600

    Revert change in helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,5 +40,62 @@ has_many :follows
 has_many :profiles, through: :follows, dependent: :destroy
 has_many :offer_saves, dependent: :destroy
 
+########################## USER HELPERS ############################
+  # creates a new heart row with post_id and user_id
+  def heart!(post)
+    self.hearts.create!(post_id: post.id)
+  end
+
+  # destroys a heart with matching post_id and user_id
+  def unheart!(post)
+    heart = self.hearts.find_by_post_id(post.id)
+    heart.destroy!
+  end
+
+  # returns true of false if a post is hearted by user
+  def heart?(post)
+    self.hearts.find_by_post_id(post.id)
+  end
+
+  # returns true of false if a post is hearted by user
+  def heart?(post)
+    self.hearts.find_by_post_id(post.id)
+  end
+
+  #returns true or dalse if the client already reviewed profile
+  def reviewed?(profile)
+    self.reviews.find_by_profile_id(profile.id)
+  end
+
+  #returns true or dalse if the client already reviewed profile
+  def valorated?(valoration)
+    self.valorations.find_by_valorarion_id(valoration.id)
+  end
+
+  ##Adding Followin methods
+  def follow_user
+    if current_client.follow!(@profile)
+      format.html { redirect_to @profile, notice: 'Has seguido a' }
+    end
+  end
+
+  def follow!(profile)
+    self.follows.create(profile_id: profile.id)
+  end
+
+  def unfollow!(profile)
+    follow = self.follows.find_by profile_id:(profile.id)
+    follow.destroy!
+  end
+
+  def follow?(profile)
+    self.follows.find_by profile_id:(profile.id)
+  end
+
+  #client has redeemed?
+  def redeemed?(coupon)
+    self.coupon_redemptions.find_by coupon_id:(coupon.id)
+  end
+
 
 end
