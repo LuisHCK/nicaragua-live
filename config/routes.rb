bc38260a6ac27630bffba08712d98854833784f5
LuisHCK <luisjcenteno@live.com>
2016-09-23 05:53:42 -0600

    Add terms of service and privacity policy

--- config/routes.rb
+++ config/routes.rb
@@ -40,33 +40,37 @@ Rails.application.routes.draw do
     omniauth_callbacks: "user/omniauth_callbacks"}
 
 
-  match 'heart',    to: 'hearts#heart', via: :post
-  match 'unheart',  to: 'hearts#unheart', via: :delete
-  match 'follow',   to: 'follows#follow', via: :post
-  match 'unfollow', to: 'follows#unfollow', via: :delete
+    match 'heart',    to: 'hearts#heart', via: :post
+    match 'unheart',  to: 'hearts#unheart', via: :delete
+    match 'follow',   to: 'follows#follow', via: :post
+    match 'unfollow', to: 'follows#unfollow', via: :delete
 
-  match 'search', to: 'menus#search', via: :get
-  match 'tools', to: 'menus#tools', via: :get
+    match 'search', to: 'menus#search', via: :get
+    match 'tools', to: 'menus#tools', via: :get
 
-  match 'admin_panel', to: 'menus#admin_panel', via: :get
-  match 'post_category', to: 'menus#post_category', via: :post
-  match 'favorites', to: 'welcome#favorites', via: :get
-  match 'install', to:'menus#install', via: :get
+    match 'admin_panel', to: 'menus#admin_panel', via: :get
+    match 'post_category', to: 'menus#post_category', via: :post
+    match 'install', to:'menus#install', via: :get
 
-  match 'redeem', to: 'coupon_redemptions#create',  via: :post
-  match 'exchange', to: 'coupon_redemptions#update',  via: :patch
+    match 'redeem', to: 'coupon_redemptions#create',  via: :post
+    match 'exchange', to: 'coupon_redemptions#update',  via: :patch
 
-  match 'close_todo', to: 'todos#close', via: :patch
-  match 'open_todo', to: 'todos#close', via: :patch
+    match 'close_todo', to: 'todos#close', via: :patch
+    match 'open_todo', to: 'todos#close', via: :patch
 
 
-  match 'bienvenido', to: 'menus#user_welcome', via: :get
-  match 'myprofile', to: 'menus#myprofile', via: :get
+    match 'bienvenido', to: 'menus#user_welcome', via: :get
+    match 'myprofile', to: 'menus#myprofile', via: :get
 
-  match 'partners_info', to: 'profiles#info', via: :get
+    match 'partners_info', to: 'profiles#info', via: :get
 
-  match '/google13e2745398afabff.html',
-      :to => proc { |env| [200, {}, ["google-site-verification: /google13e2745398afabff.html"]] }, via: :get
+    match 'favorites', to: 'welcome#favorites', via: :get
+    match 'terms', to: 'welcome#terms', via: :get
+    match 'privacitypolicy', to: 'welcome#privacity', via: :get
+
+
+    match '/google13e2745398afabff.html',
+    :to => proc { |env| [200, {}, ["google-site-verification: /google13e2745398afabff.html"]] }, via: :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
