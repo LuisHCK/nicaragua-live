ff4880bf06d8653f1aee866d0b3e5ed15762e88f
LuisHCK <luisjcenteno@live.com>
2016-09-26 22:30:54 -0600

    Move follow button to helper
    -Aditional small and Large follow button

--- app/helpers/follows_helper.rb
+++ app/helpers/follows_helper.rb
@@ -1,2 +1,25 @@
 module FollowsHelper
+	def follow_bt_lg(profile)
+		if user_signed_in?
+			if current_user.follow?(profile)
+				link_to 'Siguiendo', unfollow_path(profile_id: profile.id), remote: true,id:"follow", class:"btn btn-lg btn-default", method: :delete
+			else
+				button_to "Seguir", follow_path(profile_id: profile.id), class:"btn btn-lg btn-success",  remote: true, id:"follow"
+			end
+		else
+			link_to 'Seguir', new_user_session_path, class:"btn btn-lg btn-success"
+		end
+	end
+
+	def follow_small(profile)
+		if user_signed_in?
+			if current_user.follow?(profile)
+				link_to 'Siguiendo', unfollow_path(profile_id: profile.id), remote: true,id:"follow", class:"btn btn-default", method: :delete
+			else
+				button_to "Seguir", follow_path(profile_id: profile.id), class:"btn btn-success",  remote: true, id:"follow"
+			end
+		else
+			link_to 'Seguir', new_user_session_path, class:"btn btn-lg btn-success"
+		end
+	end
 end
