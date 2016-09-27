ee95c04706ed13351059ede7376c96bcc04cbe57
LuisHCK <luisjcenteno@live.com>
2016-09-26 22:31:22 -0600

    modify class for heart button

--- app/helpers/hearts_helper.rb
+++ app/helpers/hearts_helper.rb
@@ -3,12 +3,12 @@ module HeartsHelper
 		if user_signed_in?
 
 			if current_user.heart?(post)
-				return button_to unheart_path(post_id: post.id), remote: true, method: :delete, class:"btn-link" do
+				return button_to unheart_path(post_id: post.id), remote: true, method: :delete, class:"btn" do
 					concat icon('heart',class:'red-text', style:'font-size:2rem;')
 					concat content_tag(:span, post.hearts.count,class:'label label-info')
 				end
 			else
-				return button_to heart_path(post_id: post.id), remote: true, class:"btn-link" do
+				return button_to heart_path(post_id: post.id), remote: true, class:"btn" do
 					concat icon('heart-o',style:'font-size:2rem')
 					concat content_tag(:span, post.hearts.count,class:'label label-info')
 				end
@@ -18,7 +18,7 @@ module HeartsHelper
 				concat icon('heart',class:'red-text', style:'font-size:2rem;')
 				content_tag(:span, post.hearts.count,class:'label label-info')
 			else
-				icon('heart-o',style:'font-size:2rem')
+				icon('heart-o',style:'font-size:2rem', class:"btn")
 			end
 		end#end check user
 	end#function
