6684e3e98ba64224c0c1e4ea7499c0d4b96d9063
LuisHCK <luisjcenteno@live.com>
2016-09-28 21:53:22 -0600

    fix icon and counter for hearts

--- app/helpers/hearts_helper.rb
+++ app/helpers/hearts_helper.rb
@@ -5,21 +5,23 @@ module HeartsHelper
 			if current_user.heart?(post)
 				return button_to unheart_path(post_id: post.id), remote: true, method: :delete, class:"btn" do
 					concat icon('heart',class:'red-text', style:'font-size:2rem;')
-					concat content_tag(:span, post.hearts.count,class:'label label-info')
+					concat content_tag(:span, post.hearts.count,class:'badge')
 				end
 			else
 				return button_to heart_path(post_id: post.id), remote: true, class:"btn" do
 					concat icon('heart-o',style:'font-size:2rem')
-					concat content_tag(:span, post.hearts.count,class:'label label-info')
+					concat content_tag(:span, post.hearts.count,class:'badge')
 				end
 			end#end check for heart
 		else
 			if post.hearts.count > 0
-				concat icon('heart',class:'red-text', style:'font-size:2rem;')
-				content_tag(:span, post.hearts.count,class:'label label-info')
-			else
-				icon('heart-o',style:'font-size:2rem', class:"btn")
+				content_tag :button, class: "btn" do
+					concat(icon('heart-o',style:'font-size:2rem'))
+					concat(content_tag(:span, post.hearts.count,class:'badge'))
 			end
+		else
+			content_tag(:button, icon('heart-o',style:'font-size:2rem'), class:"btn")
+		end
 		end#end check user
 	end#function
 end#end module
