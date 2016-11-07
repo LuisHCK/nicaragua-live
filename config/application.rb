c92b5393aa0e824e7542e0088eddd926673d0d05
LuisHCK <luisjcenteno@live.com>
2016-11-06 21:49:51 -0600

    fix ajax json conflict

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,14 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Liveapp
-  class Application < Rails::Application
+	class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
+
+    config.action_dispatch.default_headers.merge!({
+    	'Access-Control-Allow-Origin' => '*',
+    	'Access-Control-Request-Method' => '*'
+    	})
+end
 end
