bbdf94aedccd7a50210eb9472e67b9f0a8b25d6d
LuisHCK <luisjcenteno@live.com>
2016-11-13 22:22:52 -0600

    add new helpers for users

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -6,4 +6,24 @@ module WelcomeHelper
 			return "#{'$'}#{offer.precio}"
 		end
 	end
+
+	def usr_avatar(user)
+		if user.profile.present?
+			return user.profile.avatar(:thumb)
+		elsif user.clientprofile.present?
+			return user.clientprofile.avatar(:thumb)
+		else
+			return "error"
+		end
+	end
+
+	def usr_name(user)
+		if user.profile.present?
+			return user.profile.name
+		elsif user.clientprofile.present?
+			return "#{user.clientprofile.first_name} #{user.clientprofile.last_name}"
+		else
+			return "error"
+		end		
+	end
 end
